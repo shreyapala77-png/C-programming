eac207258b310e0c48214acb123ba9489bd5cf45
Pala Shreya <shreyapala77@gmail.com>
2025-10-28 16:09:02 +0530

    Add ticket cost calculation program

---
+++ ticketsmaya.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+int X,total_cost;
+scanf("%d",&X);
+total_cost=4*X;
+if(total_cost <= 100) {
+printf("YES");
+}
+else{
+printf("NO");
+return 0;
+}
