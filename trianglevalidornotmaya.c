d6ea7ca352a6ceec4e544c99f07cc42ea458b26a
Pala Shreya <shreyapala77@gmail.com>
2025-10-28 18:15:34 +0530

    Create trianglevalidornotmaya.c

---
+++ trianglevalidornotmaya.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+int main ()
+{
+int a,b,c;
+scanf("%d%d%d",&a,&b,&c);
+if((a+b>c) &&(a+b>c) && (b+c>a))
+printf("Valid triangle");
+else
+printf("Invalidtriangle");
+return 0; 
+}
