6a8039e5455f3e2872fc713cd39a80b3d309d5f1
Pala Shreya <shreyapala77@gmail.com>
2025-10-28 18:18:43 +0530

    Create characterisuppercaseorlowercasemaya.c

---
+++ characterisuppercaseorlowercasemaya.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+char ch;
+scanf(" %c",&ch);
+if(ch>='A'&& ch <= 'Z'){
+printf("uppercase alphabet");
+}
+else if(ch>='a'&& ch <= 'z'){
+printf("lowercase alphabet");
+}
+else{
+printf("not an alphabet");
+}
+return 0;
+}
