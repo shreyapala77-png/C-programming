33a49855d33e4a0bcc93c77df3f1c57e77804998
Pala Shreya <shreyapala77@gmail.com>
2025-10-28 16:11:28 +0530

    Implement speed conversion from km/h to m/s
    Calculate speed in meters per second from input speed in km/h.

---
+++ kmphtomphmaya.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+int speed;
+float speed_in_meter_per_second;
+scanf("%d",&speed) ;
+speed_in_meter_per_second=(speed*5)/18.0;
+printf("%.2f",speed_in_meterper_second) ;
+return 0;
+}
