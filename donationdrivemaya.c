2b38680f966f31b96371e398668e0ba7461e6d0a
Pala Shreya <shreyapala77@gmail.com>
2025-10-28 16:03:02 +0530

    Add donation calculation program in donationdrivemaya.c

---
+++ donationdrivemaya.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+int N,X,remaining_number_of_donations;
+scanf("%d%d",&N,&X) ;
+remaining_number_of_donations=N-X;
+printf("%d",remaining_number_of_donations)
+return 0;
+}
