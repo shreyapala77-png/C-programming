849e2ab903aaff66412f414f908b6328d26b2955
Pala Shreya <shreyapala77@gmail.com>
2025-10-30 18:41:00 +0530

    Create donationrewardsmaya.c

---
+++ donationrewardsmaya.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int x;
+scanf("%d", &x);
+if(x <= 3)
+{
+printf ("BRONZE");
+}
+else if(x>3 &&x <= 6)
+{
+printf("SILVER");
+}
+else
+printf ("GOLD");
+}
+return 0;
+}
